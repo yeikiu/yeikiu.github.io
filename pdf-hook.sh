06a58ce0f802aaf84fe7c0a0bed2cfacb849da9e
JQ <jqocon@gmail.com>
2020-02-28 04:11:52 +0000

    wip: script

--- pdf-hook.sh
+++ pdf-hook.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-copyfiles src/style.css dist/style.css
+cd src
+copyfiles style.css ../dist/style.css
+cd ..
 
 # Markdown to HTML with Pandoc
 pandoc --standalone -c style.css -o dist/resume.html src/resume.md
 
 # HTML to PDF with WKhtmlToPDF
 wkhtmltopdf -L 20mm -R 20mm dist/resume.html dist/resume.pdf
-
-read x
