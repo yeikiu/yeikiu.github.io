e20497f38dc2e8988aa0c92c4abe4f33cee4f402
Yeikiu <jqocon@gmail.com>
2020-05-06 00:43:55 +0100

    Update pdf-hook.sh

--- pdf-hook.sh
+++ pdf-hook.sh
@@ -5,7 +5,7 @@ copyfiles -a '**/*' '../resume'
 cd ../resume
 
 # Markdown to HTML with Pandoc
-pandoc --standalone -c style.css -o index.html resume.md
+npx pandoc --standalone -c style.css -o index.html resume.md
 
 # HTML to PDF with WKhtmlToPDF
-wkhtmltopdf -L 20mm -R 20mm index.html resume.pdf
+npx wkhtmltopdf -L 20mm -R 20mm index.html resume.pdf
