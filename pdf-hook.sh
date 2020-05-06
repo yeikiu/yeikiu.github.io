5d4ec0cf4dca9ccec39c5a8b1cbbdacddb0f8171
JQ <jqocon@gmail.com>
2020-05-06 01:22:53 +0100

    test: GH actions

--- pdf-hook.sh
+++ pdf-hook.sh
@@ -3,12 +3,11 @@
 apt-get install wkhtmltopdf
 echo "wkhtmltopdf Installed!"
 
-# cd src
-# copyfiles -a '**/*' '../resume'
-# cd ../resume
+cp -r src/**/* resume/
+cd resume
 
 # # Markdown to HTML with Pandoc
-# npx pandoc --standalone -c style.css -o index.html resume.md
+pandoc --standalone -c style.css -o index.html resume.md
 
 # # HTML to PDF with WKhtmlToPDF
-# npx wkhtmltopdf -L 20mm -R 20mm index.html resume.pdf
+wkhtmltopdf -L 20mm -R 20mm index.html resume.pdf
